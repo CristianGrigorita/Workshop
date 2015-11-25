3145b7741ac62bedeef0bdfe409c5b04057fac08
Koding <grigoria.c@gmail.com>
2015-11-25 14:38:00 +0000

    formatare si variabile

--- print student name.c
+++ print student name.c
@@ -4,63 +4,71 @@
 
 int main()
 {
-    FILE *fp;
-    char buff[255];
-    char forname[20],name[20],mail[20],grade[5],city[10];
-    int i=0,j=0,k=0;
+	FILE *fp;
+	char buff[255];
+	char firstname[20];
+	char name[20];
+	char mail[20];
+	char grade[5];
+	char city[10];
+	int buff_counter;
+	int word_counter;
+	int letter_counter;
 
 
-    fp = fopen("students.csv", "r");
-    while(fscanf(fp, "%s", buff) > 0)
-    {
-        j=0;k=0;i=0;
-        while(*(buff+i) != 0)
-        {
-            if(buff[i] == ';')
-            {
-                j++;
-                k=0;
-            }
-            else{
-                if(j==0)
-                {
-                 a[k] = buff[i];
-                 if(buff[i+1]== ';')
-                    a[k+1]='\0';
-                }
-               if(j==1)
-                {
-                 b[k] = buff[i];
-                 if(buff[i+1]== ';')
-                    b[k+1]='\0';
-                }
-                if(j==2)
-                {
-                 c[k] = buff[i];
-                 if(buff[i+1]== ';')
-                    c[k+1]='\0';
-                }
-                if(j==3)
-                {
-                 d[k] = buff[i];
-                 if(buff[i+1]== ';')
-                    d[k+1]='\0';
-                }
-                 if(j==4)
-                {
-                 e[k] = buff[i];
-                 if(buff[i+1]== 0)
-                    e[k+1]='\0';
-                }
-                k++;
-            }
-            i++;
-        }
+	fp = fopen("students.csv", "r");
+	while(fscanf(fp, "%s", buff) > 0)
+	{
+		word_counter = 0;
+		letter_counter = 0;
+		buff_counter = 0;
+		while(*(buff + buff_counter) != 0)
+		{
+			if(buff[buff_counter] == ';')
+			{
+				word_counter++;
+				letter_counter = 0;
+			}
+			else{
+				if(word_counter == 0)
+				{
+					firstname[letter_counter] = buff[buff_counter];
+					if(buff[buff_counter + 1] == ';')
+						firstname[letter_counter + 1] = '\0';
+				}
+				if(word_counter == 1)
+				{
+					name[letter_counter] = buff[buff_counter];
+					if(buff[buff_counter + 1] == ';')
+						name[letter_counter + 1] = '\0';
+				}
+				if(word_counter == 2)
+				{
+					mail[letter_counter] = buff[buff_counter];
+					if(buff[buff_counter + 1] == ';')
+						mail[letter_counter + 1] = '\0';
+				}
+				if(word_counter == 3)
+				{
+					grade[letter_counter] = buff[buff_counter];
+					if(buff[buff_counter + 1] == ';')
+						grade[letter_counter + 1] = '\0';
+				}
+				if(word_counter == 4)
+				{
+					city[letter_counter] = buff[buff_counter];
+					if(buff[buff_counter + 1] == 0)
+						city[letter_counter + 1] = '\0';
+				}
+				letter_counter++;
+			}
+			buff_counter++;
+		}
 
-        if(strcmp(e,"Cluj") == 0 && atof(d) > 8.00)
-        {
-            printf("%s %s\n",a,b);
-        }
-    }
+		if(strcmp(city,"Cluj") == 0 && atof(grade) > 8.00)
+		{
+			printf("%s %s\n",firstname,name);
+		}
+	}
 	return 0;
 }
